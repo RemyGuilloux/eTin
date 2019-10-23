e31adfe18a18896583b42e3d6707e7c1a2737377
Louis Godlewski <louis.godlewski@gmail.com>
2019-10-23 14:46:42 +0200

    Update EtinApplicationTests.java

--- api/src/test/java/dev/louiiuol/etin/EtinApplicationTests.java
+++ api/src/test/java/dev/louiiuol/etin/EtinApplicationTests.java
@@ -8,5 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EtinApplicationTests {
-
+  @Test
+	public void contextLoads() {
+	}
 }
