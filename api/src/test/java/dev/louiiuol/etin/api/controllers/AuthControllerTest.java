8305a7cc275d0599ca2d8f5fd246e5143facc3d9
Louis Godlewski <louis.godlewski@gmail.com>
2019-10-24 15:49:40 +0200

    [CLEAN] Sonar Reconmmandations PART2

--- api/src/test/java/dev/louiiuol/etin/api/controllers/AuthControllerTest.java
+++ api/src/test/java/dev/louiiuol/etin/api/controllers/AuthControllerTest.java
@@ -4,15 +4,12 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import dev.louiiuol.etin.domain.model.dtos.request.UserRegisterDto;
-
 @Transactional
 public class AuthControllerTest {
 
     @Autowired
     AuthController controller; 
 
-    void should_Create_User(UserRegisterDto user){
-        
+    void should_Register_User(){
     }
 }
